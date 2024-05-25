7b61df63ef1662b12c8b893121b55f5d755ed4b3
risang443 <risang443@gmail.com>
2024-05-25 20:57:03 +0700

    view dashboard done

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,66 +7,57 @@
     <title>Gandaria Sepatu</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
+<body class="bg-[#A7CCED]">
   {{-- Navigasi Start --}}
-    
-<button data-drawer-target="sidebar-multi-level-sidebar" data-drawer-toggle="sidebar-multi-level-sidebar" aria-controls="sidebar-multi-level-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-  <span class="sr-only">Open sidebar</span>
-  <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-  <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-  </svg>
-</button>
 
-<aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-  <div class="h-full px-3 py-4 overflow-y-auto  bg-gray-50 dark:bg-gray-800 ">
-    <h1 class="w-full bg-slate-500 p-5 text-center font-bold rounded-md text-2xl mb-5">GANDARIA SEPATU</h1>
+<aside id="sidebar-multi-level-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 rounded-md " aria-label="Sidebar">
+  <div class="h-full px-3 py-4 overflow-y-auto  bg-[#545E75] dark:bg-gray-800 text-white">
+    <h1 class="w-full bg-[#304D6D] p-5 text-center font-bold rounded-md text-2xl mb-5">GANDARIA SEPATU</h1>
 
      <ul class="space-y-2 font-medium">
         <li>
            <a href="/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+              <svg class="w-5 h-5 text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
               </svg>
-              <span class="ms-3">Dashboard</span>
+              <span class="ms-3 text-white">Dashboard</span>
            </a>
         </li>
         <li>
-           <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-            <svg class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+           <button type="button" class="flex items-center w-full p-2 text-base text-white transition duration-75 rounded-lg group hover:bg-gray-100 hover:text-black" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+            <svg class="w-5 h-5  text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <path d="M12 7.205c4.418 0 8-1.165 8-2.602C20 3.165 16.418 2 12 2S4 3.165 4 4.603c0 1.437 3.582 2.602 8 2.602ZM12 22c4.963 0 8-1.686 8-2.603v-4.404c-.052.032-.112.06-.165.09a7.75 7.75 0 0 1-.745.387c-.193.088-.394.173-.6.253-.063.024-.124.05-.189.073a18.934 18.934 0 0 1-6.3.998c-2.135.027-4.26-.31-6.3-.998-.065-.024-.126-.05-.189-.073a10.143 10.143 0 0 1-.852-.373 7.75 7.75 0 0 1-.493-.267c-.053-.03-.113-.058-.165-.09v4.404C4 20.315 7.037 22 12 22Zm7.09-13.928a9.91 9.91 0 0 1-.6.253c-.063.025-.124.05-.189.074a18.935 18.935 0 0 1-6.3.998c-2.135.027-4.26-.31-6.3-.998-.065-.024-.126-.05-.189-.074a10.163 10.163 0 0 1-.852-.372 7.816 7.816 0 0 1-.493-.268c-.055-.03-.115-.058-.167-.09V12c0 .917 3.037 2.603 8 2.603s8-1.686 8-2.603V7.596c-.052.031-.112.059-.165.09a7.816 7.816 0 0 1-.745.386Z"/>
             </svg>
             
-                 <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Data Barang</span>
+                 <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-white hover:text-black">Data Barang</span>
                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                  </svg>
            </button>
            <ul id="dropdown-example" class="hidden py-2 space-y-2">
                  <li>
-                    <a href="/output" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Etalase Barang</a>
+                    <a href="/output" class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 hover:text-black">Etalase Barang</a>
                  </li>
                  <li>
-                    <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Penambahan Barang</a>
+                    <a href="#" class="flex items-center w-full p-2 text-white transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 hover:text-black">Penambahan Barang</a>
                  </li>
            </ul>
         </li>
         <li>
           <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg class="w-5 h-5  text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <svg class="w-5 h-5  text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
               <path fill-rule="evenodd" d="M8 3a2 2 0 0 0-2 2v3h12V5a2 2 0 0 0-2-2H8Zm-3 7a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h1v-4a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v4h1a2 2 0 0 0 2-2v-5a2 2 0 0 0-2-2H5Zm4 11a1 1 0 0 1-1-1v-4h8v4a1 1 0 0 1-1 1H9Z" clip-rule="evenodd"/>
             </svg>
-            
-
-             <span class="flex-1 ms-3 whitespace-nowrap">Print Data</span>
+             <span class="flex-1 ms-3 whitespace-nowrap text-white hover:text-black">Print Data</span>
           </a>
        </li>
         <li>
            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+              <svg class="flex-shrink-0 w-5 h-5 text-black transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
               </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+              <span class="flex-1 ms-3 whitespace-nowrap text-white">Users</span>
            </a>
         </li>
         
@@ -76,8 +67,12 @@
 
 
   {{-- Navigasi End --}}
-
-  @yield('content')  
+{{-- Isi --}}
+<div > 
+   @yield('content') 
+</div>
+   
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+{{-- Isi End --}}
 </body>
 </html>
--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -2,38 +2,72 @@
 
 @section('content')
 
-<div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 bg-slate-300">
-        {{-- Hero Dashboard Start--}}
+<div class="p-4 sm:ml-64 bg-[#A7CCED]">
+    {{-- Hero Dashboard Start--}}
+    <div class="p-4 border-l-cyan-200 bg-[#63ADF2] rounded-md">
+        
 
-        <div class="flex flex-wrap justify-center items-center ">
-            <h1 class="text-5xl flex ">GANDARIAN SEPATU</h1>
+        <div class="flex flex-wrap justify-start items-center font-sans text-white ">
+            <h1 class="text-5xl flex w-full mb-3 font-bold">GANDARIAN SEPATU</h1>
+            <p class="text-2xl flex w-full mb-2 font-medium">Selamat Datang di Dashboard Aplikasi Inventory Gandaria Sepatu</p>
+            <p class="text-2xl flex font-medium">Aplikasi ini dapat membantu anda untuk mendapatkan informasi terhadap situasi stok sepatu yang akan dijual</p>
         </div>
-
-        {{-- Hero Dashboard End --}}
     </div>
-    <div class="flex flex-wrap justify-center items-center p-8">
-    {{-- Card Section --}}
-        <div class="h-15  p-8">
-            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img class="rounded-t-lg" src="" alt="" />
-                </a>
-                <div class="p-5">
-                    <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                    </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
-                </div>
+<hr class="h-2 my-8 bg-[#82A0BC] border-0 ">
+
+    {{-- Hero Dashboard End --}}
+    <div class="grid grid-cols-2 grid-rows-2 gap-12 px-8 mt-8 ">
+        <div class="flex items-center bg-white border rounded-md overflow-hidden shadow">
+            <div class="p-4 bg-green-400"><?xml version="1.0" encoding="UTF-8"?>
+                <svg class="h-12 w-12 tex" fill="#FFFF" enable-background="new 0 0 462.68 462.68" fill="none" version="1.1" viewBox="0 0 462.68 462.68" xmlns="http://www.w3.org/2000/svg" >
+                <path d="m454.68 383.82v-30.546c0-12.481-7.382-23.769-18.807-28.756-37.15-16.212-70.676-43.134-94.401-75.807-25.378-34.948-38.792-74.831-38.792-115.34 0-10.408 0.799-20.936 2.374-31.29 1.78-11.708-3.017-23.288-12.519-30.222l-89.814-65.541c-5.072-3.701-11.279-5.207-17.481-4.235-6.202 0.969-11.655 4.295-15.355 9.366l-120.31 164.88c-2.568-1.039-5.371-1.385-8.177-0.947-4.091 0.64-7.688 2.833-10.128 6.178l-28.296 38.778c-5.037 6.904-3.519 16.619 3.385 21.657l272.68 198.98c17.834 13.015 38.931 19.894 61.01 19.894h107.13c8.547 0 15.5-6.953 15.5-15.5v-48c0-5.826-3.235-10.907-8-13.554zm-272.68-363.53c1.339-1.834 3.311-3.038 5.554-3.388 2.24-0.355 4.487 0.192 6.323 1.532l89.815 65.542c4.965 3.622 7.467 9.695 6.53 15.849-1.688 11.099-2.544 22.385-2.544 33.546 0 8.609 0.568 17.188 1.675 25.702l-23.547 7.849c-3.93 1.311-6.053 5.558-4.743 9.487 1.048 3.143 3.975 5.13 7.114 5.13 0.786 0 1.586-0.125 2.373-0.387l21.317-7.106c1.211 5.84 2.691 11.637 4.417 17.382l-22.478 7.493c-3.93 1.311-6.053 5.558-4.743 9.487 1.048 3.143 3.975 5.13 7.114 5.13 0.786 0 1.586-0.125 2.373-0.387l22.555-7.519c2.116 5.563 4.484 11.063 7.085 16.493l-23.134 7.711c-3.929 1.31-6.053 5.558-4.743 9.487 1.048 3.143 3.974 5.13 7.114 5.13 0.786 0 1.586-0.125 2.373-0.387l25.416-8.472c2.997 5.293 6.235 10.502 9.702 15.62l-25.612 8.538c-3.93 1.31-6.053 5.558-4.743 9.487 1.048 3.143 3.974 5.13 7.114 5.13 0.786 0 1.586-0.125 2.373-0.387l27.5-9.167c0.868-0.289 1.642-0.729 2.316-1.27 24.923 32.081 58.753 58.456 96.004 74.713 5.958 2.601 9.808 8.491 9.808 15.008v28.601h-99.632c-5.227 0-10.221-1.628-14.442-4.709l-263.63-192.38 120.02-164.49zm265.68 425.08c5.6843e-14 0.275-0.225 0.5-0.5 0.5h-107.13c-18.879 0-36.918-5.882-52.168-17.011l-272.68-198.98c-0.223-0.162-0.271-0.476-0.109-0.698l28.296-38.778c0.03-0.041 0.122-0.168 0.327-0.199 0.029-5e-3 0.058-7e-3 0.084-7e-3 0.154 0 0.251 0.07 0.287 0.097l272.68 198.98c6.807 4.967 14.858 7.592 23.284 7.592h107.13c0.275 0 0.5 0.225 0.5 0.5v47.999z"/>
+                </svg>
+                
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width=""
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z">
+                    </path>
+                </svg></div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Jumlah Sepatu yang ada :</h3>
+                <p class="text-3xl">12,768</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-md overflow-hidden shadow">
+            <div class="p-4 bg-blue-400"><svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none"
+                    viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2">
+                    </path>
+                </svg></div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Jumlah Jenis Sepatu :</h3>
+                <p class="text-3xl">39,265</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-md overflow-hidden shadow">
+            <div class="p-4 bg-indigo-400"><svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none"
+                    viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z">
+                    </path>
+                </svg></div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Sepatu Yang Hampir Habis :</h3>
+                <p class="text-3xl">142,334</p>
+            </div>
+        </div>
+        <div class="flex items-center bg-white border rounded-md overflow-hidden shadow">
+            <div class="p-4 bg-red-400"><svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none"
+                    viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4">
+                    </path>
+                </svg></div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">Sepatu Kehabisan Stok :</h3>
+                <p class="text-3xl">34.12%</p>
             </div>
         </div>
     </div>
-    {{-- Card Section --}}
  </div>
 @endsection
